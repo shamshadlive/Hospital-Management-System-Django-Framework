70767a163f36ef004a56ce1c7e31535ce02a3b02
shamshadlive <muhammadshamshad365@gmail.com>
2023-08-28 21:10:05 +0530

    configured the files

--- ehealthPortal/settings.py
+++ ehealthPortal/settings.py
@@ -84,19 +84,15 @@ WSGI_APPLICATION = 'ehealthPortal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ehealthportal',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT':'3306',
-        'OPTIONS': {
-        'sql_mode': 'traditional',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
